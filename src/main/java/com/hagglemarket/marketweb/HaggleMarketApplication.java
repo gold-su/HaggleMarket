7d6211263e43318d00346468ba6dfd2bbc592e2e
pds02 <pds020130@naver.com>
2025-06-18 16:31:58 +0900

    frist

---
+++ src/main/java/com/hagglemarket/marketweb/HaggleMarketApplication.java
@@ -0,0 +1,13 @@
+package com.hagglemarket.marketweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaggleMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaggleMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hagglemarket/marketweb/HaggleMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hagglemarket.marketweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaggleMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
