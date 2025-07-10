f6248d1d895a34419f1eaf5a1326e4da2629e45f
LeeGwangPyo <dl990122@naver.com>
2025-07-11 02:12:27 +0900

    상품등록

--- src/main/java/com/hagglemarket/marketweb/post/dto/PostRequestDto.java
+++ src/main/java/com/hagglemarket/marketweb/post/dto/PostRequestDto.java
@@ -3,11 +3,9 @@ package com.hagglemarket.marketweb.post.dto;
 import com.hagglemarket.marketweb.post.entity.Post;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
