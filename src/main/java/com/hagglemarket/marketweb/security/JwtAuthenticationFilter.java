72b351045f006c71e8124883dca0ac82f45355ce
pds02 <pds020130@naver.com>
2025-08-21 22:11:17 +0900

    쓸데없는 임포트 삭제

--- src/main/java/com/hagglemarket/marketweb/security/JwtAuthenticationFilter.java
+++ src/main/java/com/hagglemarket/marketweb/security/JwtAuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +14,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.slf4j.Logger;
 
 import java.io.IOException;
 @Slf4j
