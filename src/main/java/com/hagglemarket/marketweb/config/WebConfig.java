2cabcf9444a40914e9b86df98e5b06b8815c4ec2
박동수 <pds020130@gmail.com>
2025-11-04 22:05:10 +0900

    cors 허용 범위 수정

--- src/main/java/com/hagglemarket/marketweb/config/WebConfig.java
+++ src/main/java/com/hagglemarket/marketweb/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 //리액트에서 부여된 포트를 설정함
                 .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://hagglemarket-front-react.onrender.com")
                 //리액트에서 사용하게될 각각의 API들을 허용하는 함수
